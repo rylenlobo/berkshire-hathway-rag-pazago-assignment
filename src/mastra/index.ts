72ce1056c03a6dbd77fe3ace5ab70ada6db87ba3
Rylen Lobo <loborylen98334@gmail.com>
2025-06-28 17:01:32 +0530

    added md documents

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,7 +1,7 @@
 import { Mastra } from "@mastra/core/mastra";
 import { financialAnalystAgent } from "./agents/financial-analyst-agent";
-import { PgVector, PostgresStore } from "@mastra/pg";
-import { Memory } from "@mastra/memory";
+import { PgVector } from "@mastra/pg";
+
 
 const pgVector = new PgVector({
   connectionString: process.env.POSTGRES_CONNECTION_STRING || "",
